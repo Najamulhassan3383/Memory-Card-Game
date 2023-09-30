9cc7a7b460a54056c6431485c23fe2a60407c265
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-09-30 21:04:54 +0500

    Updates

--- src/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-import { MyContext } from "./Context";
-import { useContext } from "react";
-
-function Hero() {
-  const { message } = useContext(MyContext);
-  return <div className="text-4xl text-center text-white">{message}</div>;
-}
-
-export default Hero;
